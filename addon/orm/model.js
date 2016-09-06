a877192b7cf7e7b890e04bd00cfa74dac502f63c
Leooo <viet_lionel@yahoo.fr>
2016-09-06 14:46:58 +0100

    fix for phantom test

--- addon/orm/model.js
+++ addon/orm/model.js
@@ -299,7 +299,8 @@ class Model {
     Object.keys(this.belongsToAssociations).forEach(key => {
       let association = this.belongsToAssociations[key];
       let parent = this[key];
-      if (parent && parent.isNew()) {
+      // sometimes "parent" is an id..
+      if (parent && parent.isNew && parent.isNew()) {
         let fk = association.getForeignKey();
         parent.save();
         this.update(fk, parent.id);
